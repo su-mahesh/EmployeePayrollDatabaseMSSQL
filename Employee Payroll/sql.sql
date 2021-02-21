d53c4bb59e72664351caa424119646625c388380
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-22 00:22:17 +0530

    [MaheshKangude] ADDED: UptoUC11

--- Employee Payroll/sql.sql
+++ Employee Payroll/sql.sql
@@ -9,7 +9,8 @@ create Table Employee_Payroll
 ID			int	identity(1, 1),
 Name		varchar(255) NOT Null,
 Salary		Money,
-Start_Date	Date
+Start_Date	Date,
+primary key(ID)
 )
 
 --UC3 Ability to create employee table data
@@ -23,14 +24,13 @@ select * from Employee_Payroll
 
 --UC5 Ability to retrieve salary data for particular employee
 -- as well employees who joined in particular data range
-select salary from Employee_Payroll where Name = 'Terisa';
+select salary from Employee_Payroll where Name = 'Terissa';
 select * from Employee_Payroll where Start_Date	between '2018-01-01' and GETDATE();
 
 --UC6 Ability to add gender to employee payroll table
 alter table Employee_Payroll add Gender char(1)
 update Employee_Payroll set Gender = 'M' where Name = 'Charlie' or Name = 'Bil'
-update Employee_Payroll set Gender = 'M' where Name = 'Rahul' or Name = 'Nitesh' or Name= 'Mahesh'
-
+update Employee_Payroll set Gender = 'F' where Gender IS null
 --UC7 Ability to find sum avg min max and number of male female employee
 select sum(salary) as total_salary from Employee_Payroll
 select avg(salary) as average_salary from Employee_Payroll
@@ -38,14 +38,52 @@ select min(salary) as min_salary from Employee_Payroll
 select max(salary) as max_salary from Employee_Payroll
 select count(salary) as salary_count from Employee_Payroll
 
-select sum(salary) as total_salary from Employee_Payroll group by Gender
-select count(*) as salary_count from Employee_Payroll group by Gender
-select max(salary) as max_salary from Employee_Payroll group by Gender
+select Gender, sum(salary) as total_salary from Employee_Payroll group by Gender
+select Gender, count(salary) as salary_count from Employee_Payroll group by Gender
+select Gender, max(salary) as max_salary from Employee_Payroll group by Gender;
 
 --UC8 Ability to extend Employee_Payroll to store phone number, address, department
-alter table Employee_Payroll add phone_number bigint, address varchar(255) default('pune'), department varchar(255) not null default('null')
+alter table Employee_Payroll add Phone_number bigint, Address varchar(255) default('pune'), Department varchar(255) not null default('empty')
 
 --UC9 Ability to extend Employee_Payroll to store Basic Pay, Deductions, Taxable Pay, Income Tax, Net Pay
-alter table Employee_Payroll add Basic_Pay int, Deduction int, Taxable_Pay int, Income_Tax int, Net_Pay int
+alter table Employee_Payroll add Deduction int, Taxable_Pay int, Income_Tax int, Net_Pay int
+EXEC sp_RENAME 'Employee_Payroll.Salary', 'Basic_Pay', 'column'
 
+--UC10 Ability to make terissa as part of sales and marketing department 
+update Employee_Payroll set Department = 'sales'  where Name  = 'Terissa'
+insert into Employee_Payroll(Name, Department) values('Terissa', 'marketing')
+
+--UC11 Ability to implement er diagram 
+-- normalize database
+select ID, Basic_Pay, Deduction, Taxable_Pay, Income_Tax, Net_Pay into Payroll from Employee_Payroll
+select * from Payroll
+alter table Payroll add foreign key(ID) references Employee_Payroll(ID)
+alter table Employee_Payroll drop column Basic_Pay, Deduction, Taxable_Pay, Income_Tax, Net_Pay;
+
+select ID into Company from Employee_Payroll
+select * from Company
+
+select ID, Department into Department from Employee_Payroll
+alter table Department add foreign key(ID) references Employee_Payroll(ID)
+
+ALTER TABLE Employee_Payroll DROP CONSTRAINT DF__Employee___Depar__3C69FB99
+alter table Employee_Payroll drop column Department
+
+
+
+select * from DF__Employee___Depar__3C69FB99
+drop table Employee_Payroll
+drop table Payroll
+drop table Department
+drop table Company
+select * from Department
+EXEC sp_RENAME 'Employee_Payroll.Salary', 'Basic_Pay', 'column'
+alter table Employee_Payroll alter column Salary to Basic_Pay
 select * from Employee_Payroll
+alter table Employee_Payroll drop Basic_Pay
+exec sp_columns Employee_Payroll
+
+update Employee_Payroll set Gender = 'F'  where Name  = 'Terissa'
+select * from Employee_Payroll where name = 'Terissa'
+insert into Employee_Payroll (Name, department) values('Terissa', 'Marketing')
+drop table Employee_Payroll
